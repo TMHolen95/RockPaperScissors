cc7da101c3dfcecc19d2669764bf685246f15f5f
TMHolen95 <tormartin.holen@gmail.com>
2019-12-17 10:41:13 +0100

    Improved instructions on what to do.

--- app/src/main/java/com/netcompany/tormah/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/netcompany/tormah/rockpaperscissors/MainActivity.kt
@@ -9,29 +9,49 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    // TODO: create a solution using a architectural pattern MVC, MVVM, etc...
+    // TODO: create a solution using the MVVM architectural pattern
     //  There is a model and some enums in the GameModel.kt file.
-    //  Choose if you want to build upon this, or delete them and start fresh.
-    //  Create a file for the Controller or ViewModel depending on the architecture you choose.
+    //  1. Create a file for the ViewModel file .
+    //  2. Connect the Activity with the ViewModel
+    //  3. Connect the ViewModel with the GameModel
+    //  4. Update the Views in the activity to reflect hte state of the game
+    //  5. Attempt to figure out how to keep the game state when rotating the phone
+    //  For info on LiveModel see:
+    //  https://developer.android.com/topic/libraries/architecture/viewmodel
+    //  If MutableLiveData is hard, try just replacing it with a simple variable with GameData
+
+
+    // val model: MyViewModel = //TODO instantiate retrieve ViewModel
+    // Note in Kotlin values that don't get reassigned are defined with "val",
+    // if they get reassigned use "var"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO: retrieve the views and update them with useful text ex below:
+        // TODO replace with your own code
+
+        // In kotlin you can access views by their id directly (as shown a few lines below),
+        // or you can assign them to variables like this:
+        val textView = findViewById<TextView>(R.id.textView_main_myChoice)
+
+        // Example: Setting text on a view:
         textView_main_myChoice.text = GameAction.Rock.toString()
-        // An example pof changing a views visibility
+
+        // Example: Changing a views visibility:
         textView_main_playAgainInfo.visibility = View.INVISIBLE
 
+        // Example: Adding a button listener:
+        button_main_paperChosen.setOnClickListener {
+            // You can update TextViews in listeners like these
+
+            // This logs to the Logcat (Hit: Alt + 6)
+            Log.d("MainActivity","I was clicked")
+        }
 
     }
 
-    // TODO: create logic for handling user input
-    // The user interface should reflect the games two states;
-    // State 1: waiting for the user to select a action, i.e. either the rock, paper or
-    // scissors buttons.
-    // State 2; Displaying the result of the game, where the "new game" button resets
-    // the game to state 1.
+
 
 
 }
